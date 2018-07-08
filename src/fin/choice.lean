317b641b2cb968307f3c6649203d8ec95f4f54cc
François G. Dorais <fgdorais@gmail.com>
2018-07-08 16:15:29 -0400

    feat(fin.choice,tup.choice): add fin/choice.lean

---
+++ src/fin/choice.lean
@@ -0,0 +1,13 @@
+
+theorem fin.choice : Π {n : ℕ} {C : fin n → Sort*}, 
+(∀ i, nonempty (C i)) → nonempty (Π i, C i)
+| 0 _ _ := nonempty.intro (λ i, fin.elim0 i)
+| (n+1) C h := 
+  have h0 : nonempty (C 0), from h 0,
+  have hs : nonempty (Π i, C (fin.succ i)), from fin.choice (λ i, h (fin.succ i)),
+  nonempty.elim hs $ nonempty.elim h0 $ λ c0 cs, 
+  nonempty.intro $ λ i, 
+  match i with
+  | ⟨0, _⟩ := c0
+  | ⟨i+1, h⟩ := cs ⟨i, nat.lt_of_succ_lt_succ h⟩ 
+  end
--- src/tup/choice.lean
+++ src/tup/choice.lean
@@ -3,19 +3,11 @@ Copyright © 2018 François G. Dorais. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 -/
 
-import .basic .cons
+import .basic .cons fin.choice
 
-open tup
-
-lemma tup.choice {α : Type*} : ∀ {n : ℕ} {p : fin n → α → Prop}, 
-(∀ i, ∃ x : α, p i x) → (∃ xs : α ^ n, ∀ i : fin n, p i xs[i])
-| 0 _ _ := ⟨nil, (λ i, fin.elim0 i)⟩
-| (n+1) p h :=
-  let p' := λ (i : fin n) (x : α), p (fin.succ i) x in
-  have (∃ xs : α ^ n, ∀ i : fin n, p' i xs[i]),
-  from tup.choice (λ i, h (fin.succ i)),
-  exists.elim this (λ xs hxs, exists.elim (h 0) (λ x hx, 
-    exists.intro (x :: xs) (λ i, match i with
-    | ⟨0, _⟩ := hx
-    | ⟨i+1, h⟩ := hxs ⟨i, nat.lt_of_succ_lt_succ h⟩
-    end)))
+lemma tup.choice {α : Type*} {n : ℕ} {p : fin n → α → Prop} : 
+(∀ i, ∃ x : α, p i x) → (∃ xs : α ^ n, ∀ i : fin n, p i xs[i]) :=
+assume h,
+have nonempty (Π i, { x : α // p i x }),
+from fin.choice (λ i, exists.elim (h i) (λ x hx, nonempty.intro ⟨x, hx⟩)),
+nonempty.elim this (λ z, ⟨λ i, (z i).val, λ i, (z i).property⟩)
