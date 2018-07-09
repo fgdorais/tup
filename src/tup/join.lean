6a18a8197c9fec72af1de603a65161e6b97800ca
François G. Dorais <fgdorais@gmail.com>
2018-07-08 22:18:58 -0400

    feat(tup.join): add join_val

--- src/tup/join.lean
+++ src/tup/join.lean
@@ -19,3 +19,22 @@ definition ntup.join {n : ℕ} (nxs : (ntup α) ^ n) : ntup α :=
 
 @[simp]
 lemma tup.join_nil : tup.join tup.nil = @tup.nil α := rfl
+
+definition fin.step : 
+Π {m : ℕ} (ns : ℕ ^ m) (i : fin m) (j : fin ns[i]), fin (tup.sum ns)
+| 0 _ i _ := 
+  fin.elim0 i
+| (m+1) ns ⟨0,_⟩ j := 
+  fin.lift_by (tup.sum ns.tail) j
+| (m+1) ns ⟨i+1,ih⟩ j := 
+  fin.push_by ns.head (fin.step ns.tail ⟨i, nat.lt_of_succ_lt_succ ih⟩ j)
+
+lemma tup.join_val :
+∀ {m : ℕ} (nxs : (ntup α) ^ m) (i : fin m) (j : fin nxs[i].length), 
+(tup.join nxs)[fin.step (tup.map ntup.length nxs) i j] = nxs[i].to_tup[j]
+| 0 _ i _ :=
+  fin.elim0 i
+| (m+1) ns ⟨0,_⟩ j :=
+  by simp [tup.join, fin.step]; reflexivity
+| (m+1) ns ⟨i+1,_⟩ j :=
+  by simp [tup.join, fin.step, tup.join_val]; reflexivity
