f11ae74a02adfc3ad608c15142273073b5614f93
François G. Dorais <fgdorais@gmail.com>
2018-07-10 18:11:50 -0400

    fix(tup.join): fix after changes to tup.sumprod

--- src/tup/join.lean
+++ src/tup/join.lean
@@ -7,34 +7,29 @@ import .basic .cons .append .sumprod .map
 
 variable {α : Type*}
 
-@[reducible]
-definition tup.join :
-Π {n : ℕ} (nxs : (ntup α) ^ n), α ^ (tup.sum (tup.map ntup.length nxs))
-| 0 _ := tup.nil
-| (n+1) nxs := ntup.to_tup (nxs.head) ++ tup.join nxs.tail
+definition ntup.join {n : ℕ} : ntup α ^ n → ntup α := tup.join
 
-@[reducible]
-definition ntup.join {n : ℕ} (nxs : (ntup α) ^ n) : ntup α :=
-⟨tup.sum (tup.map ntup.length nxs), tup.join nxs⟩ 
+definition ntup.join_length {n : ℕ} (ts : ntup α ^ n) : ℕ := (ntup.join ts).length
 
-@[simp]
-lemma tup.join_nil : tup.join tup.nil = @tup.nil α := rfl
+#exit
 
 definition fin.step : 
-Π {m : ℕ} (ns : ℕ ^ m) (i : fin m) (j : fin ns[i]), fin (tup.sum ns)
+Π {m : ℕ} (ns : ℕ ^ m) (i : fin m) (j : fin ns[i]), fin (tup.sum' ns)
 | 0 _ i _ := 
   fin.elim0 i
 | (m+1) ns ⟨0,_⟩ j := 
-  fin.lift_by (tup.sum ns.tail) j
+  fin.lift (tup.sum' ns.tail) j
 | (m+1) ns ⟨i+1,ih⟩ j := 
-  fin.push_by ns.head (fin.step ns.tail ⟨i, nat.lt_of_succ_lt_succ ih⟩ j)
+  fin.push ns.head (fin.step ns.tail ⟨i, nat.lt_of_succ_lt_succ ih⟩ j)
 
-lemma tup.join_val :
+lemma ntup.join_val :
 ∀ {m : ℕ} (nxs : (ntup α) ^ m) (i : fin m) (j : fin nxs[i].length), 
-(tup.join nxs)[fin.step (tup.map ntup.length nxs) i j] = nxs[i].to_tup[j]
+(ntup.join nxs).to_tup[fin.step (tup.map ntup.length nxs) i j] = nxs.to_tup[i].to_tup[j]
 | 0 _ i _ :=
   fin.elim0 i
 | (m+1) ns ⟨0,_⟩ j :=
-  by simp [tup.join, fin.step]; reflexivity
+  begin 
+  simp [tup.join, fin.step]
+  end
 | (m+1) ns ⟨i+1,_⟩ j :=
   by simp [tup.join, fin.step, tup.join_val]; reflexivity
