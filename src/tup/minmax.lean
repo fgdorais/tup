e97284df528f3ba1e57fef1b720399f79c6e926f
François G. Dorais <fgdorais@gmail.com>
2018-07-10 18:24:41 -0400

    feat(tup.minmax): add missing inequalities

--- src/tup/minmax.lean
+++ src/tup/minmax.lean
@@ -27,6 +27,10 @@ lemma tup.le_max : ∀ {n : ℕ} {xs : α ^ (n+1)} {i : fin (n+1)}, xs[i] ≤ xs
   ... ≤ xs.tail.max : tup.le_max
   ... ≤ xs.max      : le_max_right xs.head xs.tail.max
 
+lemma tup.max_le : ∀ {n : ℕ} {xs : α ^ (n+1)} {y : α}, (∀ i, xs[i] ≤ y) → xs.max ≤ y
+| 0 xs y h := h 0
+| (n+1) xs y h := max_le (h 0) (tup.max_le (λ i, h (fin.succ i)))
+
 definition tup.max' : Π {n : ℕ}, n ≠ 0 → α ^ n → α
 | 0 h _ := absurd rfl h
 | (n+1) _ xs := xs.max
@@ -34,6 +38,10 @@ definition tup.max' : Π {n : ℕ}, n ≠ 0 → α ^ n → α
 lemma tup.le_max' : ∀ {n : ℕ} {xs : α ^ n} {i : fin n}, xs[i] ≤ tup.max' (fin.nonzero_of_fin i) xs
 | 0 _ i := fin.elim0 i
 | (n+1) _ _ := tup.le_max
+
+lemma tup.max'_le : ∀ {n : ℕ} {xs : α ^ n} {y : α} {h : n ≠ 0}, (∀ i, xs[i] ≤ y) → tup.max' h xs ≤ y
+| 0 _ _ h := absurd rfl h
+| (n+1) xs y _ := tup.max_le
 
 definition tup.min : Π {n : ℕ}, α ^ (n+1) → α 
 | 0 xs := xs.head
@@ -53,6 +61,10 @@ lemma tup.min_le : ∀ {n : ℕ} {xs : α ^ (n+1)} {i : fin (n+1)}, xs[i] ≥ xs
   ... ≥ xs.tail.min : tup.min_le
   ... ≥ xs.min      : min_le_right xs.head xs.tail.min
 
+lemma tup.le_min : ∀ {n : ℕ} {xs : α ^ (n+1)} {y : α}, (∀ i, y ≤ xs[i]) → y ≤ xs.min
+| 0 xs y h := h 0
+| (n+1) xs y h := le_min (h 0) (tup.le_min (λ i, h (fin.succ i)))
+
 definition tup.min' : Π {n : ℕ}, n ≠ 0 → α ^ n → α
 | 0 h _ := absurd rfl h
 | (n+1) _ xs := xs.min
@@ -60,4 +72,8 @@ definition tup.min' : Π {n : ℕ}, n ≠ 0 → α ^ n → α
 lemma tup.min'_le : ∀ {n : ℕ} {xs : α ^ n} {i : fin n}, xs[i] ≥ tup.min' (fin.nonzero_of_fin i) xs
 | 0 _ i := fin.elim0 i
 | (n+1) _ _ := tup.min_le
+
+lemma tup.le_min' : ∀ {n : ℕ} {xs : α ^ n} {y : α} {h : n ≠ 0}, (∀ i, y ≤ xs[i]) → y ≤ tup.min' h xs
+| 0 _ _ h := absurd rfl h
+| (n+1) xs y _ := tup.le_min
 
