b0ab4c37c5b876bb68be51107a78304efcabc6d7
François G. Dorais <fgdorais@gmail.com>
2018-08-08 09:30:11 -0400

    feat(fin): extra -> basic

--- src/fin/default.lean
+++ src/fin/default.lean
@@ -3,7 +3,7 @@ Copyright © 2018 François G. Dorais. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 -/
 
+import .basic
 import .cast
 import .choice
 import .decidable
-import .extra
