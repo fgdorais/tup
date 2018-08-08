7add65e6bd238ca1843ec0d13414ab586b3f69e7
François G. Dorais <fgdorais@gmail.com>
2018-08-08 09:27:54 -0400

    feat(fin.basic): add lemmas, fin.skip -> fin.lift

--- src/fin/basic.lean
+++ src/fin/basic.lean
@@ -3,30 +3,7 @@ Copyright © 2018 François G. Dorais. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 -/
 
-namespace nat
-
-lemma add_sub_cancel' (m n : ℕ) : (m + n) - m = n := 
-by rw [nat.add_comm, nat.add_sub_cancel]
-
-lemma sub_add_of_le {m n : ℕ} : m ≤ n → (n - m) + m = n :=
-assume h : m ≤ n, by rw [add_comm, add_sub_of_le h]
-
-lemma sub_lt_of_lt_add_of_ge {m n i : ℕ} : i ≥ m → i < m + n → i - m < n :=
-assume (hm : m ≤ i) (h : i < m + n),
-lt_of_add_lt_add_left $
-show m + (i - m) < m + n, 
-from calc m + (i - m) = i : add_sub_of_le hm
-              ... < m + n : h
-
-lemma le_sub_of_add_le_of_ge {m n i : ℕ} :
-n ≤ i → n + m ≤ i → m ≤ i - n :=
-assume hn h, 
-le_of_add_le_add_left $
-show n + m ≤ n + (i - n),
-from calc n + m ≤ i : h
-  ... = n + (i - n) : eq.symm $ add_sub_of_le hn
-
-end nat
+import .extra
 
 namespace fin
 
@@ -34,11 +11,14 @@ lemma nonzero_of_fin : ∀ {n : ℕ}, fin n → n ≠ 0
 | 0 i := fin.elim0 i
 | (n+1) _ := nat.succ_ne_zero n
 
+@[simp] lemma val_mk {n : ℕ} (i : ℕ) (hi : i < n) : (mk i hi).val = i := rfl
+@[simp] lemma is_lt_mk {n : ℕ} (i : ℕ) (hi : i < n) : (mk i hi).is_lt = hi := rfl
+
 definition zero (n : ℕ) : fin (n+1) := ⟨0, nat.succ_pos n⟩
 definition last (n : ℕ) : fin (n+1) := ⟨n, nat.lt_succ_self n⟩
 
-def skip {n : ℕ} : fin n → fin (n+1) := λ ⟨i, hi⟩, ⟨i, nat.lt_succ_of_lt hi⟩
-def drop {n : ℕ} : Π (i : fin (n+1)), i ≠ last n → fin n := 
+definition lift {n : ℕ} : fin n → fin (n+1) := λ ⟨i, hi⟩, ⟨i, nat.lt_succ_of_lt hi⟩
+definition drop {n : ℕ} : Π (i : fin (n+1)), i ≠ last n → fin n := 
 λ ⟨i, hi⟩ h, ⟨i, lt_of_le_of_ne (nat.le_of_lt_succ hi) (fin.vne_of_ne h)⟩
 
 @[simp] lemma zero_val {n : ℕ} : (fin.zero n).val = 0 := rfl
@@ -47,96 +27,87 @@ def drop {n : ℕ} : Π (i : fin (n+1)), i ≠ last n → fin n :=
 @[simp] lemma succ_val {n : ℕ} : ∀ (i : fin n), (fin.succ i).val = i.val + 1 := λ ⟨_,_⟩, rfl
 @[simp] lemma pred_val {n : ℕ} : ∀ (i : fin (n+1)) {h : i ≠ fin.zero n}, (fin.pred i h).val = i.val - 1 := λ ⟨_,_⟩, rfl
 
-@[simp] lemma skip_val {n : ℕ} : ∀ (i : fin n), (fin.skip i).val = i.val := λ ⟨_,_⟩, rfl
+@[simp] lemma lift_val {n : ℕ} : ∀ (i : fin n), (fin.lift i).val = i.val := λ ⟨_,_⟩, rfl
 @[simp] lemma drop_val {n : ℕ} : ∀ (i : fin (n+1)) {h : i ≠ fin.zero n}, (fin.pred i h).val = i.val - 1 := λ ⟨_,_⟩, rfl
 
 lemma succ_ne_zero {n : ℕ} : ∀ i, fin.succ i ≠ fin.zero n := λ ⟨i, hi⟩, fin.ne_of_vne (nat.succ_ne_zero i)
-lemma skip_ne_last {n : ℕ} : ∀ i, fin.skip i ≠ fin.last n := λ ⟨i, hi⟩, fin.ne_of_vne (ne_of_lt hi)
+lemma lift_ne_last {n : ℕ} : ∀ i, fin.lift i ≠ fin.last n := λ ⟨i, hi⟩, fin.ne_of_vne (ne_of_lt hi)
 
 lemma pred_succ {n : ℕ} : ∀ (i : fin n), fin.pred (fin.succ i) (fin.succ_ne_zero i) = i := λ ⟨_,_⟩, eq_of_veq $ rfl
-lemma drop_skip {n : ℕ} : ∀ (i : fin n), fin.drop (fin.skip i) (fin.skip_ne_last i) = i := λ ⟨_,_⟩, eq_of_veq $ rfl
-lemma succ_skip_eq_skip_succ {n : ℕ} : ∀ (i : fin n), fin.skip (fin.succ i) = fin.succ (fin.skip i) := λ ⟨_,_⟩, eq_of_veq $ rfl
+lemma drop_lift {n : ℕ} : ∀ (i : fin n), fin.drop (fin.lift i) (fin.lift_ne_last i) = i := λ ⟨_,_⟩, eq_of_veq $ rfl
+lemma succ_lift_eq_lift_succ {n : ℕ} : ∀ (i : fin n), fin.lift (fin.succ i) = fin.succ (fin.lift i) := λ ⟨_,_⟩, eq_of_veq $ rfl
 
-@[reducible]
-definition lift_by {m : ℕ} (n : ℕ) : fin m → fin (m+n)
-| ⟨i,hi⟩ := ⟨i, nat.lt_add_right i m n hi⟩
+definition lift_by {m : ℕ} (n : ℕ) : fin m → fin (m+n) := λ ⟨i,hi⟩, ⟨i, nat.lt_add_right i m n hi⟩
+definition push_by {n : ℕ} (m : ℕ) : fin n → fin (m+n) := λ ⟨i,h⟩, ⟨m+i, nat.add_lt_add_left h m⟩
 
-lemma lift_by_val {m n : ℕ} : 
-∀ {i : fin m}, (lift_by n i).val = i.val
-| ⟨_,_⟩ := rfl
+@[simp] lemma lift_by_val {m n : ℕ} : ∀ {i : fin m}, (lift_by n i).val = i.val := λ ⟨_,_⟩, rfl
+@[simp] lemma push_by_val {m n : ℕ} : ∀ {i : fin m}, (push_by n i).val = n + i.val := λ ⟨_,_⟩, rfl
 
-@[reducible]
-definition lift {m n : ℕ} : m ≤ n → fin m → fin n
-| h ⟨i, hi⟩ := ⟨i, lt_of_lt_of_le hi h⟩ 
-
-@[simp]
-lemma lift_val {m n : ℕ} {h : m ≤ n} :
-∀ {i : fin m}, (lift h i).val = i.val
-| ⟨i,hi⟩ := rfl
-
-@[simp]
-lemma lift_zero {m n : ℕ} {h : m+1 ≤ n+1} :
-lift h 0 = 0 := eq_of_veq $ by simp; reflexivity
-
-@[simp]
-lemma lift_succ {m n : ℕ} {h : m+1 ≤ n+1} (i : fin m) :
-lift h (fin.succ i) = fin.succ (lift (nat.le_of_succ_le_succ h) i) :=
-eq_of_veq $ by simp
-
-@[simp]
-lemma lift_lift {l m n : ℕ} {hlm : l ≤ m} {hmn : m ≤ n} : 
-∀ {i : fin l}, lift hmn (lift hlm i) = lift (le_trans hlm hmn) i :=
-λ i, eq_of_veq $ by simp
-
-@[reducible]
-definition push_by {n : ℕ} (m : ℕ) : fin n → fin (m+n)
-| ⟨i,h⟩ := ⟨m+i, nat.add_lt_add_left h m⟩
-
-@[simp]
-lemma push_by_val {m n : ℕ} : 
-∀ {i : fin m}, (push_by n i).val = n + i.val
-| ⟨_,_⟩ := rfl
-
-@[reducible]
-definition push {m n : ℕ} : m ≤ n → fin m → fin n
-| h ⟨i,hi⟩ := 
-  have (n - m) + i < n, from calc
-  (n - m) + i < (n - m) + m : nat.add_lt_add_left hi (n-m)
-          ... = n : by rw nat.sub_add_cancel h,
-  ⟨(n - m) + i, this⟩
-
-@[simp]
-lemma push_val {m n : ℕ} {h : m ≤ n} :
-∀ {i : fin m}, (push h i).val = (n - m) + i.val 
-| ⟨_,_⟩ := rfl
-
-@[simp]
-lemma push_push {l m n : ℕ} {hlm : l ≤ m} {hmn : m ≤ n} : 
-∀ {i : fin l}, push hmn (push hlm i) = push (le_trans hlm hmn) i :=
-have m - l ≤ m, from nat.sub_le m l,
-λ i, eq_of_veq $ by simp; rw [add_comm (m-l) (n-m), ←nat.add_sub_assoc hlm (n-m), nat.sub_add_of_le hmn]
-
-lemma lift_by_push_by_veq_push_by_lift_by {l m n : ℕ} : 
-∀ {i : fin l}, (lift_by m (push_by n i)).val = (push_by n (lift_by m i)).val :=
-λ ⟨i,_⟩, by simp
-
-lemma cases_zero_succ {n : ℕ} {C : fin (n+1) → Sort*} :
-C 0 → (Π i, C (fin.succ i)) → (Π i, C i)
+definition cases_zero_succ {n : ℕ} {C : fin (n+1) → Sort*} :
+C (fin.zero n) → (Π i, C (fin.succ i)) → (Π i, C i)
 | hz _ ⟨0,_⟩ := hz
 | _ hs ⟨i+1,hi⟩ := hs ⟨i, nat.lt_of_succ_lt_succ hi⟩
 
-lemma cases_zero_succ_on {n : ℕ} {C : fin (n+1) → Sort*} (i : fin (n+1)) :
+@[simp] lemma cases_zero {n : ℕ} {C : fin (n+1) → Sort*} (h : C 0) (hs : Π i, C (fin.succ i)) :
+cases_zero_succ h hs (fin.zero n) = h := rfl
+
+@[simp] lemma cases_succ {n : ℕ} {C : fin (n+1) → Sort*} (h : C 0) (hs : Π i, C (fin.succ i)) :
+∀ i, cases_zero_succ h hs (fin.succ i) = hs i | ⟨_,_⟩ := rfl
+
+@[reducible] definition cases_zero_succ_on {n : ℕ} {C : fin (n+1) → Sort*} (i : fin (n+1)) :
 C 0 → (Π i, C (fin.succ i)) → C i :=
 λ hz hs, fin.cases_zero_succ hz hs i
 
-lemma rec_zero_succ {C : Π n, fin n → Sort*} :
-(Π n, C (n+1) 0) → (Π n i, C n i → C (n+1) (fin.succ i)) → (Π n i, C n i) :=
+definition rec_zero_succ {C : Π {n : ℕ}, fin n → Sort*} :
+(Π n, C (fin.zero n)) → (Π {n : ℕ} (i : fin n), C i → C (fin.succ i)) → (Π {n : ℕ} (i : fin n), C i) :=
 λ hz hs n, nat.rec_on n (λ i, fin.elim0 i) $
-λ n ih, cases_zero_succ (hz n) (λ i, hs n i (ih i))
+λ n ih, cases_zero_succ (hz n) (λ i, hs i (ih i))
 
-lemma rec_zero_succ_on {C : Π {n : ℕ}, fin n → Sort*} {n : ℕ} (i : fin n) :
+@[simp] lemma rec_zero {C : Π {n : ℕ}, fin n → Sort*} (h : Π n, C (fin.zero n)) (hs : Π {n : ℕ} (i : fin n), C i → C (fin.succ i)) :
+∀ (n : ℕ), rec_zero_succ h @hs (fin.zero n) = h n | _ := rfl
+
+@[simp] lemma rec_succ {C : Π {n : ℕ}, fin n → Sort*} (h : Π n, C (fin.zero n)) (hs : Π {n : ℕ} (i : fin n), C i → C (fin.succ i)) :
+∀ {n : ℕ} (i : fin n), rec_zero_succ h @hs (fin.succ i) = hs i (rec_zero_succ h @hs i) | _ ⟨_,_⟩ := rfl
+
+@[reducible] definition rec_zero_succ_on {C : Π {n : ℕ}, fin n → Sort*} {n : ℕ} (i : fin n) :
 (Π (n : ℕ), C (fin.zero n)) → (Π (n : ℕ) (i : fin n), C i → C (fin.succ i)) → C i :=
-λ hz hs, rec_zero_succ hz hs n i
+λ hz hs, rec_zero_succ hz hs i
+
+definition cases_lift_last {n : ℕ} {C : fin (n+1) → Sort*} :
+(Π i, C (fin.lift i)) → C (fin.last n) → (Π i, C i) :=
+λ hl h ⟨i,hi⟩,
+if hin : i = n then 
+have fin.mk i hi = fin.last n, from fin.eq_of_veq hin,
+show C ⟨i,hi⟩, from eq.rec_on (eq.symm this) h 
+else
+let j := fin.mk i (lt_of_le_of_ne (nat.le_of_lt_succ hi) hin) in
+have fin.mk i hi = fin.lift j, from fin.eq_of_veq rfl,
+show C ⟨i,hi⟩, from eq.rec_on (eq.symm this) (hl j)
+
+@[simp] lemma cases_lift {n : ℕ} {C : fin (n+1) → Sort*} (hl : Π i, C (fin.lift i)) (h : C (fin.last n)) :
+∀ i, cases_lift_last hl h (fin.lift i) = hl i | ⟨i,hi⟩ := dif_neg (ne_of_lt hi)
+
+@[simp] lemma cases_last {n : ℕ} {C : fin (n+1) → Sort*} (hl : Π i, C (fin.lift i)) (h : C (fin.last n)) :
+cases_lift_last hl h (fin.last n) = h := dif_pos rfl
+
+@[reducible] definition cases_lift_last_on {n : ℕ} {C : fin (n+1) → Sort*} (i : fin (n+1)) :
+(Π i, C (fin.lift i)) → C (fin.last n) → C i :=
+λ hl h, fin.cases_lift_last hl h i
+
+definition rec_lift_last {C : Π {n : ℕ}, fin n → Sort*} :
+(Π {n : ℕ} (i : fin n), C i → C (fin.lift i)) → (Π n, C (fin.last n)) → (Π {n : ℕ} (i : fin n), C i) :=
+λ hnl hn n, nat.rec_on n (λ i, fin.elim0 i) $
+λ n ih, cases_lift_last (λ i, hnl i (ih i)) (hn n)
+
+@[simp] lemma rec_lift {C : Π {n : ℕ}, fin n → Sort*} {hl : Π {n : ℕ} (i : fin n), C i → C (fin.lift i)} {h : Π n, C (fin.last n)} :
+∀ {n : ℕ} (i : fin n), rec_lift_last @hl h (fin.lift i) = hl i (rec_lift_last @hl h i) | _ ⟨i,hi⟩ := dif_neg (ne_of_lt hi)
+
+@[simp] lemma rec_last {C : Π {n : ℕ}, fin n → Sort*} {hl : Π {n : ℕ} (i : fin n), C i → C (fin.lift i)} {h : Π n, C (fin.last n)} :
+∀ (n : ℕ), rec_lift_last @hl h (fin.last n) = h n | _ := dif_pos rfl
+
+@[reducible] definition rec_lift_last_on {C : Π {n : ℕ}, fin n → Sort*} {n : ℕ} (i : fin n) :
+(Π (n : ℕ) (i : fin n), C i → C (fin.lift i)) → (Π (n : ℕ), C (fin.last n)) → C i :=
+λ hl h, rec_lift_last hl h i
 
 end fin
 
