17ab85b686f9e339dec943e3a970c802c1e512c6
François G. Dorais <fgdorais@gmail.com>
2018-08-13 14:32:16 -0400

    feat(fin.rec): move from fin.basic to fin.rec

--- src/fin/basic.lean
+++ src/fin/basic.lean
@@ -101,34 +101,4 @@ rw ← h at h',
 exact h' i.is_lt,
 end
 
-definition cases_zero_succ {n : ℕ} {C : fin (n+1) → Sort*} :
-C (fin.zero n) → (Π i, C (fin.succ i)) → (Π i, C i)
-| hz _ ⟨0,_⟩ := hz
-| _ hs ⟨i+1,hi⟩ := hs ⟨i, nat.lt_of_succ_lt_succ hi⟩
-
-@[simp] lemma cases_zero {n : ℕ} {C : fin (n+1) → Sort*} (h : C 0) (hs : Π i, C (fin.succ i)) :
-cases_zero_succ h hs (fin.zero n) = h := rfl
-
-@[simp] lemma cases_succ {n : ℕ} {C : fin (n+1) → Sort*} (h : C 0) (hs : Π i, C (fin.succ i)) :
-∀ i, cases_zero_succ h hs (fin.succ i) = hs i | ⟨_,_⟩ := rfl
-
-@[reducible] definition cases_zero_succ_on {n : ℕ} {C : fin (n+1) → Sort*} (i : fin (n+1)) :
-C 0 → (Π i, C (fin.succ i)) → C i :=
-λ hz hs, fin.cases_zero_succ hz hs i
-
-definition rec_zero_succ {C : Π {n : ℕ}, fin n → Sort*} :
-(Π n, C (fin.zero n)) → (Π {n : ℕ} (i : fin n), C i → C (fin.succ i)) → (Π {n : ℕ} (i : fin n), C i) :=
-λ hz hs n, nat.rec_on n (λ i, fin.elim0 i) $
-λ n ih, cases_zero_succ (hz n) (λ i, hs i (ih i))
-
-@[simp] lemma rec_zero {C : Π {n : ℕ}, fin n → Sort*} (h : Π n, C (fin.zero n)) (hs : Π {n : ℕ} (i : fin n), C i → C (fin.succ i)) :
-∀ (n : ℕ), rec_zero_succ h @hs (fin.zero n) = h n | _ := rfl
-
-@[simp] lemma rec_succ {C : Π {n : ℕ}, fin n → Sort*} (h : Π n, C (fin.zero n)) (hs : Π {n : ℕ} (i : fin n), C i → C (fin.succ i)) :
-∀ {n : ℕ} (i : fin n), rec_zero_succ h @hs (fin.succ i) = hs i (rec_zero_succ h @hs i) | _ ⟨_,_⟩ := rfl
-
-@[reducible] definition rec_zero_succ_on {C : Π {n : ℕ}, fin n → Sort*} {n : ℕ} (i : fin n) :
-(Π (n : ℕ), C (fin.zero n)) → (Π (n : ℕ) (i : fin n), C i → C (fin.succ i)) → C i :=
-λ hz hs, rec_zero_succ hz hs i
-
 end fin
